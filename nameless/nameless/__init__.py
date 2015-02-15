80c6af6d9ebb2f661e1a9ecba93493eed31c474a
ruthgrace <ruthgracewong@gmail.com>
2015-02-15 04:35:53 -0500

    got file upload working (not combined iwth databse yet

--- nameless/nameless/__init__.py
+++ nameless/nameless/__init__.py
@@ -1,12 +1,13 @@
 import os
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, send_from_directory, Request
 from werkzeug import secure_filename
 from flask.ext.mongoengine import MongoEngine
 
-UPLOAD_FOLDER = '/resumeStorage'
+UPLOAD_FOLDER = './resumeStorage'
 ALLOWED_EXTENSIONS = set(['pdf','html'])
 
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 app.config["MONGODB_SETTINGS"] = {'DB': "resumeDB"}
 app.config["SECRET_KEY"] = "KeepThisS3cr3t"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -24,3 +25,32 @@ def register_blueprints(app):
     app.register_blueprint(admin)
 
 register_blueprints(app)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
